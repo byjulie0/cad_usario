d5680c2651d5892e91996f01601a929d80e98e94
byjulie0 <julievieiramartins0@gmail.com>
2025-08-13 17:05:21 -0400

    Update spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -8,14 +8,63 @@ describe('template spec', () => {
     cy.wait(500)
     cy.get('#btn_login').click()
 
-    const usuarios = [];
-    for (let i = 1; i <= 10; i++){
-      usuarios.push({
-        id: i,
-        nome: `usuario ${i}`,
-        senha: `senha${i}`
-      });
-    }
+
+    const user_json = `[
+      {
+        "id": 1,
+        "nome": "Julie",
+        "senha": "122"
+      },
+      {
+        "id": 2,
+        "nome": "Maria Helena",
+        "senha": "147"
+      },
+      {
+        "id": 3,
+        "nome": "Anna Laurah",
+        "senha": "456"
+      },
+      {
+        "id": 4,
+        "nome": "4651651",
+        "senha": "789"
+      },
+      {
+        "id": 5,
+        "nome": "lara",
+        "senha": "852"
+      },
+      {
+        "id": 6,
+        "nome": "Fernanda",
+        "senha": "369"
+      },
+      {
+        "id": 7,
+        "nome": "Camila Regina",
+        "senha": "753"
+      },
+      {
+        "id": 8,
+        "nome": "Juliana",
+        "senha": "951"
+      },
+      {
+        "id": 9,
+        "nome": "Isabella",
+        "senha": "279"
+      },
+      {
+        "id": 10,
+        "nome": "Miriely",
+        "senha": "436"
+      }
+    
+    ]`;
+
+    const usuarios = JSON.parse(user_json);
+    
     cy.get('body')
     usuarios.forEach((usuario, index) => {
       cy.get('#nome').type(usuario.nome)
